13ea50d87f1bfb90262701be0cf6f51f58fc36c3
Hakimuddin Kika <hakimuddin.kika92@gmail.com>
2024-03-13 23:26:29 +0300

    Excalidraw Integration

---
+++ components/ExcalidrawComp.jsx
@@ -0,0 +1,19 @@
+"use client";
+import { Excalidraw, convertToExcalidrawElements } from "@excalidraw/excalidraw";
+
+
+const ExcalidrawWrapper = () => {
+  console.info(convertToExcalidrawElements([{
+    type: "rectangle",
+    id: "rect-1",
+    width: 186.47265625,
+    height: 141.9765625,
+  },]));
+  return (
+    <div style={{height:"85vh", width:"85vw"}}>  
+      <Excalidraw />
+    </div> 
+  );
+};
+
+export default ExcalidrawWrapper;
